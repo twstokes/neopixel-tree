63f0c75caa13f2efe4745d19eb527bff84f6ff8b
Tanner W. Stokes <tanner@tannr.com>
2022-12-03 11:55:18 -0500

    Break out sequences, connect via WiFi and start a UDP server.

--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -1,112 +1,83 @@
 #include "Arduino.h"
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
 #include <Adafruit_NeoPixel.h>
 
+#include "sequences.h"
+#include "wifi_config.h"
+
 #define PIN D1
+#define PIXEL_COUNT 106
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(106, PIN, NEO_GRB + NEO_KHZ800);
+WiFiUDP Udp;
+#define UDP_PORT 8733
+char packet[255];
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
+/*
+  TODO:
+    Brightness command
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
+    Sequences:
+      - off
+      - color wipe
+      - theater chase
+      - rainbow
+      - rainbow cycle
+      - theater chase rainbow
+      - variable speeds of sequences that take a delay parameter
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
+*/
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  // strip.setBrightness(10);
+
+  // set the strip to orange before establing a wifi connection
+  strip.fill(strip.Color(strip.gamma8(255), strip.gamma8(87), strip.gamma8(51)));
+  strip.show();
+
+  WiFi.config(ip, gateway, subnet);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // give it a moment to connect by running a theater chase sequence
+    theaterChase(&strip, strip.Color(strip.gamma8(255), strip.gamma8(87), strip.gamma8(51)), 50);
+  }
+
+  // wifi is good - go green
+  strip.fill(strip.Color(strip.gamma8(0), strip.gamma8(255), strip.gamma8(0)));
+  strip.show();
+  delay(1000);
+
+  Udp.begin(UDP_PORT);
 }
 
 void loop() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    int len = Udp.read(packet, 255);
+    if (len > 0) {
+      
+    }
+  }
+
+
   // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  // colorWipe(&strip, strip.Color(255, 0, 0), 50); // Red
+  // colorWipe(&strip, strip.Color(0, 255, 0), 50); // Green
+  // colorWipe(&strip, strip.Color(0, 0, 255), 50); // Blue
 
   // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127, 0, 0), 50); // Red
-  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  // theaterChase(&strip, strip.Color(127, 127, 127), 50); // White
+  // theaterChase(&strip, strip.Color(127, 0, 0), 50); // Red
+  // theaterChase(&strip, strip.Color(0, 0, 127), 50); // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  // rainbow(&strip, 20);
+  rainbowCycle(&strip, 20);
+  // theaterChaseRainbow(&strip, 50);
 }
---
+++ mcu/src/sequences.h
@@ -0,0 +1,9 @@
+
+
+
+
+void theaterChaseRainbow(Adafruit_NeoPixel *strip, uint8_t wait);
+void theaterChase(Adafruit_NeoPixel *strip, uint32_t c, uint8_t wait);
+void colorWipe(Adafruit_NeoPixel *strip, uint32_t c, uint8_t wait);
+void rainbow(Adafruit_NeoPixel *strip, uint8_t wait);
+void rainbowCycle(Adafruit_NeoPixel *strip, uint8_t wait);
