be0d1b8713a937142c44fff8dd3915e81d5fcdb7
Tanner W. Stokes <tanner@tannr.com>
2022-12-04 21:16:19 -0500

    Use unpacking function.

--- mcu/src/commands.cpp
+++ mcu/src/commands.cpp
@@ -58,7 +58,7 @@ void fill_color_cmd(uint8_t *data, Adafruit_NeoPixel *strip) {
 }
 
 // converts an RGB byte array to an array of packed colors
-void load_colors_from_data(uint8_t *data, uint8_t num_colors, uint32_t *colors) {
+void unpack_colors_from_data(uint8_t *data, uint8_t num_colors, uint32_t *colors) {
     uint8_t *chan_ptr = data;
     for (int i=0; i<num_colors; i++) {
         colors[i] = Adafruit_NeoPixel::Color(*chan_ptr, *(chan_ptr + 1), *(chan_ptr + 2));
@@ -69,22 +69,14 @@ void load_colors_from_data(uint8_t *data, uint8_t num_colors, uint32_t *colors) 
 // fills the strip by repeating the provided color(s)
 // uses gamma correction
 void fill_pattern_cmd(uint8_t *data, Adafruit_NeoPixel *strip) {
-    // rgb - 1 byte per channel
-    const uint8_t num_channels = 3;
     // the first byte is the number of colors provided
     // note: not the total number of bytes
     const uint8_t num_colors = data[0];
     // numer of provided colors can't exceed number of pixels
     if (num_colors > strip->numPixels()) return;
     // stores all the colors provided
-    uint32_t c[num_colors * num_channels];
-    // pointer to first color channel
-    uint8_t *chan_ptr = &data[1];
-    for (int i=0; i<num_colors; i++) {
-        uint32_t color = strip->Color(*chan_ptr, *(chan_ptr + 1), *(chan_ptr + 2));
-        c[i] = strip->gamma32(color);
-        chan_ptr += num_channels;
-    }
+    uint32_t c[num_colors * 3];
+    unpack_colors_from_data(&data[1], num_colors, c);
     fill_pattern(c, num_colors, strip);
 }
 
