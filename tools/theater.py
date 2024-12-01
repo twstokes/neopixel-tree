dd5ddf1a15fdad0698e5515eff0b851eae069344
Tanner W. Stokes <tanner@tannr.com>
2024-12-01 11:01:20 -0500

    Add repeat.

--- tools/theater.py
+++ tools/theater.py
@@ -3,7 +3,8 @@ from config import UDP_IP, UDP_PORT
 import commands
 
 command = [commands.theater_chase]
-data = [0, 0, 255, 0]
+# repeat, color
+data = [1, 0, 255, 0]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes(command + data), (UDP_IP, UDP_PORT))
