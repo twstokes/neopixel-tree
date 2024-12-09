bc440f3ec34dabe558a90260fcd71deaa19bb1f9
Tanner W. Stokes <tanner@tannr.com>
2024-12-08 21:31:53 -0500

    Support sending delay.

--- tools/rainbow_repeat.py
+++ tools/rainbow_repeat.py
@@ -4,7 +4,13 @@ import commands
 
 command = [commands.rainbow]
 repeat = [1]
+# in milliseconds
+delay = 6000
+
+delay_high = (delay >> 8) & 0xFF
+delay_low = delay & 0xFF
+delay_bytes = [delay_high, delay_low]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(command + repeat), (UDP_IP, UDP_PORT))
+sock.sendto(bytes(command + repeat + delay_bytes), (UDP_IP, UDP_PORT))
 
